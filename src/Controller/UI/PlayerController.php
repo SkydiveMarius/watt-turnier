6f7dfbdbf31c57bbe854339cd17143a8ae2358bb
Marius Meissner <info@marius-meissner.net>
2017-08-23 20:50:54 +0200

    Connect UI Player controller with repository

--- src/Controller/UI/PlayerController.php
+++ src/Controller/UI/PlayerController.php
@@ -1,6 +1,8 @@
 <?php
 namespace FCT\Watten\Src\Controller\UI;
 
+use FCT\Watten\Src\Team\TeamRepository;
+
 /**
  * Class PlayerController
  *
@@ -9,10 +11,28 @@ namespace FCT\Watten\Src\Controller\UI;
 class PlayerController extends UiController
 {
     /**
+     * @var TeamRepository
+     */
+    private $repository;
+
+    /**
+     * PlayerController constructor.
+     *
+     * @param TeamRepository $repository
+     */
+    public function __construct(TeamRepository $repository = null)
+    {
+        $this->repository = $repository ?: new TeamRepository();
+    }
+
+    /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function show()
     {
-        return $this->render('players.twig', ['navigationItem' => 'players']);
+        return $this->render('players.twig', [
+            'navigationItem' => 'players',
+            'teams'          => $this->repository->getAll()
+        ]);
     }
 }

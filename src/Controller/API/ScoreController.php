f96bf38816a1c3848a5dea630a951b5839f23e36
Marius Meissner <info@marius-meissner.net>
2017-08-23 23:53:59 +0200

    Added empty score controller

---
+++ src/Controller/API/ScoreController.php
@@ -0,0 +1,12 @@
+<?php
+namespace FCT\Watten\Src\Controller\API;
+
+/**
+ * Class ScoreController
+ *
+ * @package FCT\Watten\Src\Controller\API
+ */
+class ScoreController extends APIController
+{
+
+}

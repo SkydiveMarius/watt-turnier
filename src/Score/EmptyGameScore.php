9b9040567de7dc33f88a31738ac8e8271dd7aeb8
Marius Meissner <marius.meissner@sixt.com>
2017-08-24 17:23:00 +0200

    Added score calculation + frontend rendering

---
+++ src/Score/EmptyGameScore.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marius
+ * Date: 24.08.17
+ * Time: 14:43
+ */
+
+namespace FCT\Watten\Src\Score;
+
+
+class EmptyGameScore
+{
+
+}
---
+++ tests/Score/GameScoreTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marius
+ * Date: 24.08.17
+ * Time: 16:47
+ */
+
+namespace FCT\Watten\Tests\Score;
+
+
+class GameScoreTest
+{
+
+}
